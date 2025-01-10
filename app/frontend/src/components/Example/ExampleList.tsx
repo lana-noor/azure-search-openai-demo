e8e518b124ae0ffb08693dbabf9dd5ae6e89fa40
lana-noor <lananoor@microsoft.com>
2025-01-10 14:21:27 +0400

    examplelist6

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -3,14 +3,14 @@ import { Example } from "./Example";
 import styles from "./Example.module.css";
 
 const DEFAULT_EXAMPLES: string[] = [
-    "What are the essential procedures for obtaining a license to operate a home healthcare facility in Dubai?",
-    "Explain the primary goal of the Clinical Governance Framework outlined by DHA.",
-    "What are the key licensure requirements for establishing a dental laboratory that I need to follow?"
+    "What are the procedures for replacing faulty or damaged communication devices, and what costs are employees responsible for?",
+    "For PROMIS, explain the process of submitting standing instructions?",
+    "What are the detailed steps involved in the vessel berthing process at Container Terminal 01?"
 ];
 const GPT4V_EXAMPLES: string[] = [
-    "What are the essential procedures for obtaining a license to operate a home healthcare facility in Dubai?",
-    "Explain the primary goal of the Clinical Governance Framework outlined by DHA.",
-    "What are the key licensure requirements for establishing a dental laboratory that I need to follow?"
+    "What are the procedures for replacing faulty or damaged communication devices, and what costs are employees responsible for?",
+    "For PROMIS, explain the process of submitting standing instructions?",
+    "What are the detailed steps involved in the vessel berthing process at Container Terminal 01?"
 ];
 
 interface Props {
